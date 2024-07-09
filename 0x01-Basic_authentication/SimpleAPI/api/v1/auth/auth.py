ce690599d435f4150322513f50a4acf0b257057e
imane <imanezenasnii@gmail.com>
2024-07-09 14:37:31 +0100

    Add basic auth project

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" """
+""" Auth module"""
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
-    """ """
+    """ Auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ """
+        """ require_auth method"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
 
@@ -20,9 +20,9 @@ class Auth:
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ """
+        """ authorization header method """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ """
+        """ current_user method"""
         return None
