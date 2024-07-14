07ae8ea5f3b673ee250f99ea8f9ac593e74ef1f1
Aiimey <132620366+Aiimeey@users.noreply.github.com>
2024-07-14 10:19:24 +0100

    Update auth.py

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -21,7 +21,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ authorization header method """
-        return None
+        if request is None:
+            return None
+        auth_header = request.headers.get('Authorization')
+        if not auth_header:
+            return None
+        return auth_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user method"""
