d21e62cf49a64d0779c100a3a944973566be9c02
imane <imanezenasnii@gmail.com>
2024-12-16 08:43:56 +0100

    Apply corrections

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,6 +51,9 @@ def logout():
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
+            # resp = make_response(redirect('/'))
+            # resp.delete_cookie('session_id') 
+            # return resp
             return redirect('/')
     abort(403)
 
